8eb57a43cf17b2af1f6a3dba2090d946d75e7671
Timur Bakiiev <volkprizrak124@gmail.com>
2025-02-06 16:03:33 +0100

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+// Оголошення функції makeTransaction
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+// Перевірка функції
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
